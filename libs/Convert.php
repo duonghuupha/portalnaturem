6464c852714786dffffd5a338265d0d2947d68ea
duonghuupha <duonghuupha@hotmail.com.vn>
2023-08-20 00:46:18 +0700

    ádf

--- libs/Convert.php
+++ libs/Convert.php
@@ -26,6 +26,107 @@ class Convert{
         return $newfilename;
     }
 
+    // hien thi danh sach destination
+	function convert_number_to_words($number){
+    	$hyphen = ' ';
+    	$conjunction = '  ';
+    	$separator = ' ';
+    	$negative = 'âm ';
+    	$decimal = ' phẩy ';
+    	$dictionary = array(
+    		0 => 'Không',
+    		1 => 'Một',
+    		2 => 'Hai',
+    		3 => 'Ba',
+    		4 => 'Bốn',
+    		5 => 'Năm',
+    		6 => 'Sáu',
+    		7 => 'Bảy',
+    		8 => 'Tám',
+    		9 => 'Chín',
+    		10 => 'Mười',
+    		11 => 'Mười một',
+    		12 => 'Mười hai',
+    		13 => 'Mười ba',
+    		14 => 'Mười bốn',
+    		15 => 'Mười năm',
+    		16 => 'Mười sáu',
+    		17 => 'Mười bảy',
+    		18 => 'Mười tám',
+    		19 => 'Mười chín',
+    		20 => 'Hai mươi',
+    		30 => 'Ba mươi',
+    		40 => 'Bốn mươi',
+    		50 => 'Năm mươi',
+    		60 => 'Sáu mươi',
+    		70 => 'Bảy mươi',
+    		80 => 'Tám mươi',
+    		90 => 'Chín mươi',
+    		100 => 'trăm',
+    		1000 => 'nghìn',
+    		1000000 => 'triệu',
+    		1000000000 => 'tỷ',
+    		1000000000000 => 'nghìn tỷ',
+    		1000000000000000 => 'nghìn triệu triệu',
+    		1000000000000000000 => 'tỷ tỷ'
+    	);
+    	if( !is_numeric( $number ) ){
+    		return false;
+    	}
+    	if( ($number >= 0 && (int)$number < 0) || (int)$number < 0 - PHP_INT_MAX ){
+    		// overflow
+    		trigger_error( 'convert_number_to_words only accepts numbers between -' . PHP_INT_MAX . ' and ' . PHP_INT_MAX, E_USER_WARNING );
+    		return false;
+    	}
+    	if( $number < 0 ){
+    		return $negative . $this->convert_number_to_words( abs( $number ) );
+    	}
+    	$string = $fraction = null;
+    	if( strpos( $number, '.' ) !== false ){
+    		list( $number, $fraction ) = explode( '.', $number );
+    	}
+    	switch (true){
+    		case $number < 21:
+    			$string = $dictionary[$number];
+    			break;
+    		case $number < 100:
+    			$tens = ((int)($number / 10)) * 10;
+    			$units = $number % 10;
+    			$string = $dictionary[$tens];
+    			if( $units ){
+    				$string .= $hyphen . $dictionary[$units];
+    			}
+    			break;
+    		case $number < 1000:
+    			$hundreds = $number / 100;
+    			$remainder = $number % 100;
+    			$string = $dictionary[$hundreds] . ' ' . $dictionary[100];
+    			if( $remainder ){
+    				$string .= $conjunction . $this->convert_number_to_words( $remainder );
+    			}
+    			break;
+    		default:
+    			$baseUnit = pow( 1000, floor( log( $number, 1000 ) ) );
+    			$numBaseUnits = (int)($number / $baseUnit);
+    			$remainder = $number % $baseUnit;
+    			$string = $this->convert_number_to_words( $numBaseUnits ) . ' ' . $dictionary[$baseUnit];
+    			if( $remainder ){
+    				$string .= $remainder < 100 ? $conjunction : $separator;
+    				$string .= $this->convert_number_to_words( $remainder );
+    			}
+    			break;
+    	}
+    	if( null !== $fraction && is_numeric( $fraction ) ){
+    		$string .= $decimal;
+    		$words = array( );
+    		foreach( str_split((string) $fraction) as $number ){
+    			$words[] = $dictionary[$number];
+    		}
+    		$string .= implode( ' ', $words );
+    	}
+    	return $string;
+    }
+
 	//pagination
     function pagination($total, $get_pages, $per_page = 20){
         $perpage = $per_page;
@@ -152,6 +253,125 @@ class Convert{
         return $html;
     }
 
+    function createLinks_parameter_other($total, $rows, $currentpage, $event, $parameter, $links = 7) {
+        $last = ceil( $total / $rows );
+        $start = ( ( $currentpage - $links ) > 0 ) ? $currentpage - $links : 1;
+        $end = ( ( $currentpage + $links ) < $last ) ? $currentpage + $links : $last;
+
+        $html = '';
+        $class = ( $currentpage == 1 ) ? "active" : "";
+
+        if ( $start > 1 ) {
+            $html .= '<li class="paginate_button"><a href="javascript:void(0)" onclick="'.$event.'(1, '.$parameter.')">1</a></li>';
+            $html .= '<li class="paginate_button">';
+            $html .= '<a>...</a></li>';
+        }
+        for ( $i = $start ; $i <= $end; $i++ ) {
+            $class = ( $currentpage == $i ) ? "active" : "";
+            $html .= '<li class="paginate_button ' . $class . '">';
+            $html .= '<a href="javascript:void(0)" onclick="'.$event.'('.$i.', '.$parameter.')">' . $i . '</a>';
+            $html .= '</li>';
+        }
+        if ( $end < $last ) {
+            $html .= '<li class="paginate_button">';
+            $html .= '<a >...</a></li>';
+            $html .= '<li>';
+            $html .= '<a href="javascript:void(0)" onclick="'.$event.'('.$last.', '.$parameter.')">' . $last . '</a>';
+            $html .= '</li>';
+        }
+        return $html;
+    }
+
+    function daysInWeek($weekNum){
+        $result = array();
+        $datetime = new DateTime('00:00:00');
+        $datetime->setISODate((int)$datetime->format('o'), $weekNum, 1);
+        $interval = new DateInterval('P1D');
+        $week = new DatePeriod($datetime, $interval, 6);
+        foreach($week as $day){
+            $result[] = $day->format('D d-m-Y');
+        }
+        return $result;
+    }
+
+    function return_day_text($text){
+        if($text == 'Mon'){
+            $string = 'Thứ hai';
+        }elseif($text == 'Tue'){
+            $string = 'Thứ ba';
+        }elseif($text == 'Wed'){
+            $string = 'Thứ tư';
+        }elseif($text == 'Thu'){
+            $string = 'Thứ năm';
+        }elseif($text == 'Fri'){
+            $string = 'Thứ sáu';
+        }elseif($text == 'Sat'){
+            $string = 'Thứ bảy';
+        }elseif($text == 'Sun'){
+            $string = 'Chủ nhật';
+        }
+        return $string;
+    }
+
+    function generateBarcode($data, $folder) {
+        $PNG_TEMP_DIR = DIR_UPLOAD.'/barcode/'.$folder.'/';
+        $PNG_WEB_DIR = DIR_UPLOAD.'/barcode/'.$folder.'/';
+        $SKU = $data["sku"];
+        $filename = $PNG_TEMP_DIR.$SKU.'.png';
+        if(file_exists($filename)){
+            return $filename;
+        }else{
+            $productData = $SKU;
+            $barcode = new \Com\Tecnick\Barcode\Barcode();
+            $bobj = $barcode->getBarcodeObj('C128B', "{$productData}", 450, 70, 'black', array(0, 0, 0, 0));
+            $imageData = $bobj->getPngData();
+            file_put_contents($filename, $imageData);
+            return $filename;
+        }
+    }
+
+    function generateBarcode_book($data, $folder) {
+        $PNG_TEMP_DIR = DIR_UPLOAD.'/barcode/'.$folder.'/';
+        $PNG_WEB_DIR = DIR_UPLOAD.'/barcode/'.$folder.'/';
+        $SKU = $data["sku"]; $title_file = str_replace(".", "_", $data['sku']);
+        $filename = $PNG_TEMP_DIR.$title_file.'.png';
+        if(file_exists($filename)){
+            return $filename;
+        }else{
+            $productData = $SKU;
+            $barcode = new \Com\Tecnick\Barcode\Barcode();
+            $bobj = $barcode->getBarcodeObj('C128B', "{$productData}", 450, 70, 'black', array(0, 0, 0, 0));
+            $imageData = $bobj->getPngData();
+            file_put_contents($filename, $imageData);
+            return $filename;
+        }
+    }
+
+    function generateBarcode_device($data, $folder) {
+        $PNG_TEMP_DIR = DIR_UPLOAD.'/assets/'.$folder.'/';
+        $PNG_WEB_DIR = DIR_UPLOAD.'/assets/'.$folder.'/';
+        $SKU = $data["sku"]; $title_file = str_replace(".", "_", $data['sku']);
+        $filename = $PNG_TEMP_DIR.$title_file.'.png';
+        if(file_exists($filename)){
+            return $filename;
+        }else{
+            $productData = $SKU;
+            $barcode = new \Com\Tecnick\Barcode\Barcode();
+            $bobj = $barcode->getBarcodeObj('C128B', "{$productData}", 450, 70, 'black', array(0, 0, 0, 0));
+            $imageData = $bobj->getPngData();
+            file_put_contents($filename, $imageData);
+            return $filename;
+        }
+    }
+
+    function return_between_hours($diff){
+        $years = floor($diff / (365*60*60*24));
+        $months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
+        $days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+        $hours = floor(($diff - $years * 365*60*60*24- $months*30*60*60*24 - $days*60*60*24) / (60*60));
+        return $hours;
+    }
+
     function return_show_entries($total, $perpage, $pages){
         if($total == 0){
             $html = "Hiển thị 0 đến 0 của 0 bản ghi";
@@ -167,6 +387,91 @@ class Convert{
             $html .= ' của '.$total.' bản ghi';
         }
         return $html;
+    }
+
+    function emailValid($string){ 
+        if (preg_match ("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+\.[A-Za-z]{2,6}$/", $string)){
+            return true; 
+        }else{
+            return false;
+        }
+    } 
+
+    function return_fullname_sort($txt){
+        $data = trim($txt); $data = explode(" ", $data);
+        $fullname = array_slice($data, -2,  2, true);
+        return implode(" ",  $fullname);
+    }
+
+    // cat chuoi
+    function cut($str, $len){
+        $str = trim($str);
+        if (strlen($str) <= $len) return $str;
+        $str = substr($str, 0, $len);
+        if ($str != "") {
+            if (!substr_count($str, " ")) return $str." ...";
+            while (strlen($str) && ($str[strlen($str) - 1] != " ")) $str = substr($str, 0, -1);
+            $str = substr($str, 0, -1)." ...";
+        }
+        return $str;
+    }
+
+    function return_title_function($str){
+        $array = array(1 => "Thêm mới", 2 => "Cập nhật", 3 => "Xóa", 4 => "Nhập từ file", 
+                        5 => "Xuất dữ liệu", 6 => "Đặt trước", 7 => 'Duyệt yêu cầu');
+        if($str != ''){
+            $data = explode(",", $str);
+            foreach($data as $row){
+                $arr_title[] = $array[$row];
+            }
+            return implode(", ",  $arr_title);
+        }else{
+            return '';
+        }
+    }
+
+    function return_roles($mainid, $id, $str, $idh){
+        $array = array(1 => "Thêm mới", 2 => "Cập nhật", 3 => "Xóa", 4 => "Nhập từ file", 
+                        5 => "Xuất dữ liệu", 6 => "Đặt trước", 7 => 'Duyệt yêu cầu');
+        $data = explode(",", $str);
+        $html = ''; $sql = new Model();
+        foreach($data as $row){
+            $checked = ($idh != 0 && $sql->checked_role($idh, $id.'_'.$row) != 0) ? 'checked=""' : '';
+            $html .= '
+            <li class="tree-item" role="treeitem"> 
+                <span class="tree-item-name"> 
+                    <span class="tree-label">'.$array[$row].'</span>
+                    <input id="role_'.$mainid.'_'.$id.'_'.$row.'" name="role_'.$mainid.'_'.$id.'_" type="checkbox"
+                    value="'.$id.'_'.$row.'" onclick="set_checked_sub('.$mainid.', '.$id.', '.$row.')"
+                    '.$checked.' data_role="role_'.$mainid.'_"/>
+                </span> 
+            </li>
+            ';
+        }
+        return $html;
+    }
+
+    function return_roles_horizontal($mainid, $id, $str, $idh){
+        $array = array(1 => "Thêm mới", 2 => "Cập nhật", 3 => "Xóa", 4 => "Nhập từ file", 
+                        5 => "Xuất dữ liệu", 6 => "Đặt trước", 7 => 'Duyệt yêu cầu'); $html = ''; 
+        if($str != ''){
+            $data = explode(",", $str);
+            $sql = new Model();
+            foreach($data as $row){
+                $checked = ($idh != 0 && $sql->checked_role($idh, $id.'_'.$row) != 0) ? 'checked=""' : '';
+                $html .= '
+                <div class="col-sm-4">
+                    <span class="tree-label">'.$array[$row].'</span>
+                    <input id="role_'.$mainid.'_'.$id.'_'.$row.'" name="role_'.$mainid.'_'.$id.'_" type="checkbox"
+                    value="'.$id.'_'.$row.'" onclick="set_checked_sub('.$mainid.', '.$id.', '.$row.')"
+                    '.$checked.' data_role="role_'.$mainid.'_"/>
+                </div>
+                ';
+            }
+            return $html;
+        }else{
+            return $html;
+        }
     }
 }
 ?>
