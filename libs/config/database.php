1fa4c4599e6111a199456611c50db4c67a89eb83
duonghuupha <duonghuupha@hotmail.com.vn>
2023-12-09 16:01:43 +0700

    sdfgsdfg

--- libs/config/database.php
+++ libs/config/database.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'naturem');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_NAME', 'naturem_db');
+define('DB_USER', 'naturem_db');
+define('DB_PASS', 'HCI8hxXEM[_l');
 ?>
--- libs/config/paths.php
+++ libs/config/paths.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 define('URL', 'http://'.$_SERVER['HTTP_HOST']);
-define('URL_FRONT', 'http://naturem:81');
+define('URL_FRONT', 'http://naturem.us');
 $dirtionary = realpath($_SERVER['DOCUMENT_ROOT']);
 define('DIR_UPLOAD', $dirtionary.'/public');
 define('DIR_BASIC', $dirtionary);
